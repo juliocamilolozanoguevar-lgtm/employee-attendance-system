6289c4c12760f8a955f1ae63d6009d31a61f1056
Julio_Lozano <juliocamilolozanoguevara@gmail.com>
2026-04-25 15:30:51 -0500

    Update login.php

--- app/auth/login.php
+++ app/auth/login.php
@@ -1,9 +1,12 @@
+<?php
+require_once '../config/config.php';
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo TITLE_BUSINESS; ?></title>
 </head>
 <body>
     
