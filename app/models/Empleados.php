cd3788189821833fbc794ce780b645eb7c78ad5d
Gabriel-2415 <1680465@SENATI.PE>
2026-05-08 16:21:49 -0500

    asdf

---
+++ app/models/Empleados.php
@@ -0,0 +1,19 @@
+<?php
+//llamamos a la conexión de la base de datos.
+require_once __DIR__ . '/../core/Database.php';
+
+//creamos un modelo o clase llamada empleados (Singular).
+class empleado{
+     // La propiedad $db guardará la conexión PDO.
+    // Le decimos que solo puede ser de tipo PDO (tipado estricto).
+    // "private" significa que solo se puede usar dentro de esta clase.
+    private PDO $db;
+
+    //Al crear el modelo, obtenemos la conexion automaticamente.
+    public function __construct(){
+        // Database::getConnection() nos regresa la conexión PDO que creamos en core/Database.php.
+        // Al guardarla en $this->db, cualquier método de esta clase puede usarla.
+        $this->db = Database::getConnection();
+    }
+
+}
