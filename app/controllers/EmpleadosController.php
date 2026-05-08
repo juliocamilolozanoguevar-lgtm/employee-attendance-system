5c07c37392716295a0548b84c94d8a050d408cb9
Gabriel-2415 <1680465@SENATI.PE>
2026-05-08 16:47:43 -0500

    afga

--- app/controllers/EmpleadosController.php
+++ app/controllers/EmpleadosController.php
@@ -5,14 +5,15 @@ require_once __DIR__ . '/../core/Empleado.php';
 // Controlador para el módulo de empleados.
 class EmpleadosController extends Controller {
 
-    // Instanciamos el objeto de la clase empleados
-    $modelo = new Empleado();
-    $empleados = $modelo->getAll();
+   
     // Método por defecto. 
     public function index(): void {
+         // Instanciamos el objeto de la clase empleados
+    $modelo = new Empleado(),
+    $variable_empleados = $modelo->getAll();
         $this->view('empleados/reportes',[
             'usuario' => $_SESSION['usuario'],
-            'empleados' => $_
+            'empleados' => $variable_empleados
         ]);
     }
 
